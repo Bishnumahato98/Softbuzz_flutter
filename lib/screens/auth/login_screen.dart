d18ceda07ebb13720050299f626a73b0b73a1344
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-01-02 18:51:56 +0545

    changes in login

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -20,7 +20,7 @@
 //           mainAxisAlignment: MainAxisAlignment.center,
 //           children: [
 //             Text(
-//               "Softbuzz",
+//               "Crickbuzz",
 //               style: TextStyle(
 //                 fontFamily: 'Poppins',
 //                 fontSize: 36,
@@ -76,7 +76,7 @@
 //                     "Sign Up",
 //                     style: TextStyle(
 //                       color: Colors.lightBlueAccent,
-//
+//                       fontWeight: FontWeight.bold,
 //                     ),
 //                   ),
 //                 ),
