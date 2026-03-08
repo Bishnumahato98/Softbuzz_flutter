5ee1b24c682a54be249969790e67f175a60d22a8
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-08 11:49:55 +0545

    changes

--- lib/core/providers/shared_prefs_provider.dart
+++ lib/core/providers/shared_prefs_provider.dart
@@ -3,8 +3,3 @@
 // import '../services/storage/storage_service.dart';
 
 // // ====================== SHARED PREFERENCES PROVIDER ======================
-
-// /// Must be overridden in main.dart before runApp()
-// final storageServiceProvider = Provider<StorageService>((ref) {
-//   throw UnimplementedError('storageServiceProvider must be overridden');
-// });
