1f0eada1ca745ffd95241c0664f63264a370e50c
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-08 11:59:56 +0545

    channnn

--- lib/features/auth/data/repositories/auth_repository.dart
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -7,6 +7,7 @@ import 'package:softbuzz_app/features/auth/data/models/auth_hive_model.dart';
 import 'package:softbuzz_app/features/auth/domain/entities/auth_entity.dart';
 import 'package:softbuzz_app/features/auth/domain/repositories/auth_repository.dart';
 
+// write
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
   final authDatasource = ref.read(authLocalDatasourceProvider);
   return AuthRepository(authDatasource: authDatasource);
