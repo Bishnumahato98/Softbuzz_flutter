9c7018d6194d879cf1f6abc98b6dbf65dcf411c8
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-08 12:06:13 +0545

    channnn

--- lib/core/constants/hive_table_constant.dart
+++ lib/core/constants/hive_table_constant.dart
@@ -3,7 +3,6 @@ class HiveTableConstant {
 
   static const String dbName = "smartnews_db";
 
-  // NOTE: Tables -> Box : Index
   static const int authTypeId = 0;
   static const String authTable = "auth_table";
 
